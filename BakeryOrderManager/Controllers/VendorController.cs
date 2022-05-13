003c704310b22a61d5a8c3a86ef02e0f5afd523a
William Rodriguez <william@rodriguez1.com>
2022-05-13 11:37:37 -0700

    Add routes to vendor controller

--- BakeryOrderManager/Controllers/VendorController.cs
+++ BakeryOrderManager/Controllers/VendorController.cs
@@ -8,7 +8,26 @@ namespace BakeryOrderManager.Controllers
 		[HttpGet("/vendors")]
 		public ActionResult Index()
 		{
+			return View(Vendor.GetVendors());
+		}
+
+		[HttpGet("/vendors/new")]
+		public ActionResult New()
+		{
 			return View();
+		}
+
+		[HttpGet("vendors/{vendorName}")]
+		public ActionResult Show(string vendorName)
+		{
+			return View(Vendor.FindVendor(vendorName));
+		}
+
+		[HttpPost("/vendors")]
+		public ActionResult New(string name, string desc)
+		{
+			new Vendor(name, desc);
+			return RedirectToAction("Index");
 		}
 	}
 }
